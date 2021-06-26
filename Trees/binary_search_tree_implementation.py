5c3d707cac3ad484954f48bd396949f27b30373d
Abdul-Mukit <abdul.mukit64@gmail.com>
2021-06-25 22:36:07 -0500

    introduced binary_search_tree implementation script

---
+++ Trees/binary_search_tree_implementation.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+class BinarySearchTree():
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        print("Function not implemented")
+
+    def lookup(self, value):
+        print("Function not implemented")
+
+
+tree = BinarySearchTree()
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
