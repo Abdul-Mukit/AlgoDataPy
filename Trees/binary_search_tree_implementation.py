c664b0bc7091f7f5be0834e9a31c0d9f10afa26a
Abdul-Mukit <abdul.mukit64@gmail.com>
2021-06-29 11:43:00 -0500

    completed insert() in binary search tree

--- Trees/binary_search_tree_implementation.py
+++ Trees/binary_search_tree_implementation.py
@@ -4,22 +4,50 @@ class Node:
         self.right = None
         self.value = value
 
+    def __str__(self):
+        return str(self.__dict__)
+
+
 class BinarySearchTree():
     def __init__(self):
         self.root = None
 
     def insert(self, value):
-        print("Function not implemented")
+        return_val = True
+
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            cur_node = self.root
+            while cur_node is not None: # Traverse Tree
+                if value < cur_node.value: # go left?
+                    if cur_node.left is None:
+                        cur_node.left = Node(value)
+                        break
+                    else:
+                        cur_node = cur_node.left
+                elif value > cur_node.value: # go right?
+                    if cur_node.right is None:
+                        cur_node.right = Node(value)
+                        break
+                    else:
+                        cur_node = cur_node.right
+                else: # same value entered?`
+                    return_val = False
+                    print("Can't input value that already exists")
+        return return_val
 
     def lookup(self, value):
         print("Function not implemented")
 
 
-tree = BinarySearchTree()
-tree.insert(9)
-tree.insert(4)
-tree.insert(6)
-tree.insert(20)
-tree.insert(170)
-tree.insert(15)
-tree.insert(1)
+myTree = BinarySearchTree()
+myTree.insert(9)
+myTree.insert(4)
+myTree.insert(6)
+myTree.insert(20)
+myTree.insert(170)
+myTree.insert(15)
+myTree.insert(1)
+print("Hello")
+
