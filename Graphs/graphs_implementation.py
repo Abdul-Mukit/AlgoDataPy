75d36d1e4009452381c635e56de53ba0186b45de
Abdul-Mukit <abdul.mukit64@gmail.com>
2021-06-30 11:59:11 -0500

    implemented add_edge in graph implementation. Tested program.

--- Graphs/graphs_implementation.py
+++ Graphs/graphs_implementation.py
@@ -17,13 +17,32 @@ class MyGraph():
             return False
 
     def add_edge(self, node1, node2):
-        print("Not implemented exception")
-
-    def show_connections(self):
-        print("Not implemented exception")
+        if node1 in self.adjacentList.keys() and node2 in self.adjacentList.keys():
+            self.adjacentList[node1].append(node2)
+            self.adjacentList[node2].append(node1)
+            print("Added edge: {}".format([node1,node2]))
+            return True
+        else:
+            print("Error: node(s) {} don't exists".format([node1, node2]))
+            return False
 
 myGraph = MyGraph()
-myGraph.add_vertex(0)
-myGraph.add_vertex(1)
-myGraph.add_vertex(0)
+myGraph.add_vertex('0')
+myGraph.add_vertex('1')
+myGraph.add_vertex('2')
+myGraph.add_vertex('3')
+myGraph.add_vertex('4')
+myGraph.add_vertex('5')
+myGraph.add_vertex('6')
+
+myGraph.add_edge('3', '1');
+myGraph.add_edge('3', '4');
+myGraph.add_edge('4', '2');
+myGraph.add_edge('4', '5');
+myGraph.add_edge('1', '2');
+myGraph.add_edge('1', '0');
+myGraph.add_edge('0', '2');
+myGraph.add_edge('6', '5');
+
+
 print(myGraph)
